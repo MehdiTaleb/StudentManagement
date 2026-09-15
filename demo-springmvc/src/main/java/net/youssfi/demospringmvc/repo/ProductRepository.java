b5752ac3ad658e5e1bf34fa24711d83c63388ed7
EL MEHDI TALEB <121876699+MehdiTaleb@users.noreply.github.com>
2026-09-15 20:03:47 +0100

    Delete demo-springmvc directory

--- demo-springmvc/src/main/java/net/youssfi/demospringmvc/repo/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package net.youssfi.demospringmvc.repo;
-
-import net.youssfi.demospringmvc.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
